9c734223beb86bdf4d5b1d96a93eeb2219de41f3
karkan20 <kansara.karans@gmail.com>
2022-03-02 13:27:14 -0500

    Countdown at load

--- effect.js
+++ effect.js
@@ -1,8 +1,40 @@
 $(window).load(function () {
 	$('.loading').fadeIn('linear');
- 	//$('.container-fluid').fadeOut('linear');
+	$('.countdown').fadeIn('linear');
+	//$('.container-fluid').fadeOut('linear');
 });
 $('document').ready(function () {
+
+	var end = new Date('04/14/2022 10:0 AM');
+
+	var _second = 1000;
+	var _minute = _second * 60;
+	var _hour = _minute * 60;
+	var _day = _hour * 24;
+	var timer;
+
+	function showRemaining() {
+		var now = new Date();
+		var distance = end - now;
+		if (distance < 0) {
+
+			clearInterval(timer);
+			document.getElementById('countdown').innerHTML = 'Ceremony has concluded!!';
+
+			return;
+		}
+		var days = Math.floor(distance / _day);
+		var hours = Math.floor((distance % _day) / _hour);
+		var minutes = Math.floor((distance % _hour) / _minute);
+		var seconds = Math.floor((distance % _minute) / _second);
+
+		document.getElementById('countdown').innerHTML = days + ' Days Remaining to celebrate...';
+		// document.getElementById('countdown').innerHTML += hours + 'hrs ';
+		// document.getElementById('countdown').innerHTML += minutes + 'mins ';
+		// document.getElementById('countdown').innerHTML += seconds + 'secs';
+	}
+
+	timer = setInterval(showRemaining, 0);
 
 	$('#bulb_yellow').addClass('bulb-glow-yellow');
 	$('#bulb_red').addClass('bulb-glow-red');
@@ -20,10 +52,11 @@ $('document').ready(function () {
 		$('#bulb_pink').addClass('bulb-glow-pink-after');
 		$('#bulb_orange').addClass('bulb-glow-orange-after');
 		$('body').css('background-color', 'peachpuff');
+		$('.countdown').fadeOut('linear');
 		$('.loading').fadeOut('linear');
 		$('.container').fadeIn('linear');
-	}, 3000);
-	
+	}, 5000);
+
 });
 
 
